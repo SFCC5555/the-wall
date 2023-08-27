7e2d0ec09fb31db86beedfc612d4f08f13b56e81
sfcc5555 <sfcc.5555@gmail.com>
2023-08-27 15:20:54 -0500

    update responsive design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch  } from 'react-redux'
+import { useDispatch  } from 'react-redux'
 import logo from '../assets/images/the-wall-row.png'
 import { changeMessageStatus } from '../redux/messageSlice';
 
@@ -7,7 +7,7 @@ const NavBar = () => {
   const dispatch = useDispatch();
 
   return (
-    <nav className="fixed flex items-center gap-8 w-full p-3 bg-white border-b-2 border-black" >
+    <nav className="fixed flex items-center gap-4 sm:gap-8 w-full p-3 bg-white border-b-2 border-black" >
         <img src={logo} alt='THE WALL' className='w-28' />
         <button onClick={()=>dispatch(changeMessageStatus(true))} className="flex items-center gap-3 border-2 border-gray-200 hover:border-black p-2 font-semibold italic" >Write a message <i className="bi bi-pencil-fill" /></button>
     </nav>
